ea4d5e71f1fc0af15045435c727e374234ade9a7
jtmccr1 <mccrone@umich.edu>
2015-12-30 15:32:20 -0500

    Pipeline Miseq and Hiseq1
    The initial analysis for the  Miseq and Hiseq runs as well as the setup for the second Hiseq run testing duplicates.
    [ticket: X]

---
+++ data/process/2015-11-14/variantPipeline.bpipe.config.groovy
@@ -0,0 +1,6 @@
+REFERENCE="/scratch/alauring_fluxm/mccrone/Benchmarking_paper/data/reference/wsn33_wt_plasmid"
+REFERENCE_FA="/scratch/alauring_fluxm/mccrone/Benchmarking_paper/data/reference/wsn33_wt_plasmid.fa"
+SCRIPTS="/home/mccrone/variant_pipeline/scripts"
+LIBRARY_LOCATION="/home/mccrone/variant_pipeline/lib"
+CONTROL="PC"
+DISP="two.sided"
